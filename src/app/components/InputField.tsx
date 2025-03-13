7b6fd40a2600b84c7ee71408a3fe4b3f051cc8d5
Pitiwat Promnimit <pitiwat.p@ooca.co>
2025-03-13 22:31:15 +0700

    feat: enhance HomePage with search functionality and integrate InputField component

--- src/app/components/InputField.tsx
+++ src/app/components/InputField.tsx
@@ -10,7 +10,7 @@ export default function InputField({ ...props }: InputFieldProps) {
   return (
     <Input
       className={clsx(
-        "border-default placeholder-default mt-3 block w-full rounded-lg border-1 bg-white px-3 py-1.5 text-sm/6 text-black",
+        "border-default placeholder-default block w-full rounded-lg border-1 bg-white px-3 py-1.5 text-sm/6 text-black",
         "focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25",
         className,
       )}
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,42 +4,15 @@ import Button from "../components/Button";
 import Postcard from "../components/Postcard";
 import DropDown, { DropDownItem } from "../components/Dropdown";
 import { useState } from "react";
+import InputField from "../components/InputField";
+import clsx from "clsx";
 
 export default function HomePage() {
-  const [selectCategory, setSelectCategory] = useState<DropDownItem | null>();
-
-  const items: DropDownItem[] = [
-    { id: 1, name: "Trending" },
-    { id: 2, name: "New" },
-    { id: 3, name: "Top" },
-  ];
-
   return (
     <div className="bg-grey-100 mx-auto max-w-[798px] p-3 lg:pb-10">
-      <div className="mt-6 mb-5 flex items-center justify-between">
-        <div className="cursor-pointer lg:hidden">
-          <Image
-            src="/search-icon.svg"
-            alt="search"
-            width={24}
-            height={24}
-            priority
-          />
-        </div>
-        <div className="flex gap-3">
-          <DropDown items={items} onSelect={setSelectCategory} />
-          <Button
-            className="h-10 w-28"
-            color="success"
-            variant="solid"
-            rounded="sm"
-            size="sm"
-          >
-            Create +
-          </Button>
-        </div>
+      <div className="mt-6 mb-5">
+        <Action />
       </div>
-
       <div className="overflow-hidden rounded-2xl">
         {Array.from({ length: 10 }).map((_, index) => (
           <div key={index}>
@@ -54,6 +27,73 @@ export default function HomePage() {
           </div>
         ))}
       </div>
+    </div>
+  );
+}
+
+function Action() {
+  const [selectCategory, setSelectCategory] = useState<DropDownItem | null>();
+  const [isOpenSearch, setIsOpenSearch] = useState(false);
+
+  const items: DropDownItem[] = [
+    { id: 1, name: "Trending" },
+    { id: 2, name: "New" },
+    { id: 3, name: "Top" },
+  ];
+
+  return (
+    <div className="flex items-center justify-between gap-9">
+      <>
+        <div
+          className={clsx("cursor-pointer lg:hidden", isOpenSearch && "hidden")}
+        >
+          <Image
+            src="/search-icon.svg"
+            alt="search"
+            width={24}
+            height={24}
+            priority
+            onClick={() => setIsOpenSearch(true)}
+          />
+        </div>
+        <div className={clsx("w-full relative", isOpenSearch ? "block" : "hidden lg:block")}>
+          <div className="absolute left-3 top-1/2 h-5 w-5 -translate-y-1/2 text-gray-400" >
+            <Image
+              src="/search-icon.svg"
+              alt="search"
+              width={24}
+              height={24}
+              priority
+              onClick={() => setIsOpenSearch(true)}
+            />
+          </div>
+          <InputField
+          className="!bg-transparent placeholder-search pl-10"
+          placeholder="Search"
+            onBlur={() => setIsOpenSearch(false)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") setIsOpenSearch(false);
+            }}
+            onChange={(e) => {
+              if (e.target.value?.length == 0) setIsOpenSearch(false);
+            }}
+          />
+        </div>
+      </>
+      {!isOpenSearch && (
+        <div className="flex gap-3 lg:gap-6">
+          <DropDown items={items} onSelect={setSelectCategory} />
+          <Button
+            className="h-10 w-28"
+            color="success"
+            variant="solid"
+            rounded="sm"
+            size="sm"
+          >
+            Create +
+          </Button>
+        </div>
+      )}
     </div>
   );
 }
